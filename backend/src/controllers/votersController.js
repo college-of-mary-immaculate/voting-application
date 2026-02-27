4b3f5e9f6a55c1c02f70803ae9d3178352bd7db6
jisami19 <shisuifrancis0@gmail.com>
2026-02-28 00:49:40 +0800

    votersController fix

--- backend/src/controllers/votersController.js
+++ backend/src/controllers/votersController.js
@@ -3,34 +3,26 @@ const VoterService = require('../services/voterService');
 class VoterController {
     static async register(req, res) {
         try {
-            const {fullname, email, password} = req.body;
+            const { fullname, email, password } = req.body;
             const result = await VoterService.create(fullname, email, password);
             res.status(201).json(result);
         }
         catch (err) {
             console.error(`Voter registration error ${err.message}`);
-            res.status(400).json({error: err.message});
+            res.status(400).json({ error: err.message });
         }
     }
-    static async login(email, password) {
-            if(!email || !password) {
-                throw new Error('Email and password are required.');
-            }
-            let sql = `SELECT * FROM voters WHERE email = ?`;
-            const user = await DBService.read(sql, [email]);
-    
-            if (user.length === 0) {
-                throw new Error('Invalid email or password.');
-            }
-    
-            const voter = user[0];
-            const match = await bcrypt.compare(password, voter.password_hash);
-    
-            if (!match) {
-                throw new Error('Invalid email or password.');
-            }
-            return {message: "Login successful"};
+    static async login(req, res) {
+        try {
+            const { email, password } = req.body;
+            const result = await VoterService.login(email, password);
+            res.status(200).json(result);
         }
+        catch (err) {
+            console.error(`Voter login error ${err.message}`);
+            res.status(400).json({ error: err.message });
+        }
+    }
 }
 
 module.exports = VoterController;

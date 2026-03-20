d93310e98f7512feacc2fae7bb6583dd11776396
BrianOrea00 <deathscytheiv@gmail.com>
2026-03-20 09:32:41 +0800

    Admin Fetch Fixed!

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const AdminController = require("../controllers/adminController");
 const auth = require("../middleware/authMiddleware");
 
-router.get("/", auth.authMiddleware, auth.adminOnly, AdminController.getById);
-router.get("/:id", auth.authMiddleware, auth.adminOnly, AdminController.getAll);
+router.get("/", auth.authMiddleware, auth.adminOnly, AdminController.getAll);
+router.get("/:id", auth.authMiddleware, auth.adminOnly, AdminController.getById);
 router.post("/create",  AdminController.create);
 router.put("/:id", auth.authMiddleware, auth.adminOnly,AdminController.update);
-router.delete("/:id", auth.authMiddleware, auth.adminOnly, AdminController.delete);
-
+router.delete("/:id", auth.authMiddleware, auth.adminOnly, AdminController.delete)
 router.post("/election/:id/assign-voter",  AdminController.assignVoter);
 router.post("/election/:electionId/assign-voters", AdminController.bulkAssignVoters);
 module.exports = router;
